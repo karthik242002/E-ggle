570e1a2d29c467d725cbcaa29b88ad370752a45f
karthik242002 <karthiksiva153@gmail.com>
2023-01-12 14:34:02 +0530

    create body section

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -14,7 +14,7 @@ export default function signin({providers}) {
                     <img
                     className="w-52 object-cover"
                     
-                    src="https://cdn.dribbble.com/users/612987/screenshots/9500879/media/e18977f318a312dba4bcaf052e0a4147.jpg?compress=1&resize=400x300&vertical=top" alt="E-ggle-logo" />
+                    src= "https://pbs.twimg.com/media/FmQO8zDaEAA8vdm?format=png&name=900x900" alt="E-ggle-logo" />
                     <p className="text-sm italic my-10 text-center">This website is created for learning purposes</p>
                     <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={()=>signIn(provider.id,{callbackUrl: "/" })}> Sign in with {provider.name}</button>
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Header from '../components/Header'
+import { SearchIcon, MicrophoneIcon} from "@heroicons/react/solid";
+
 
 export default function Home() {
   return (
@@ -18,6 +21,26 @@ export default function Home() {
 
 
       {/* Body */}
+
+      <form className="flex flex-col items-center mt-40">
+        <Image
+         width="300"
+         object-Fit="cover"
+          height="100" 
+          src= "https://pbs.twimg.com/media/FmQO8zDaEAA8vdm?format=png&name=900x900" />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon  className="h-5 text-gray-500 mr-3"  />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+
+        </div>
+        <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+        <button className="btn">Google Search</button>
+        <button className="btn">I'm Feeling Lucky</button>
+
+        </div>
+       
+      </form>
 
 
       {/* Footer */}
