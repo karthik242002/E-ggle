255014f54e5da39b7317fe9fb9d8f6f608502516
karthik242002 <karthiksiva153@gmail.com>
2023-01-15 02:00:51 +0530

    create search header options component

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -3,16 +3,16 @@ import { useRouter } from "next/router";
 import { useRef } from "react";
 import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import User from "./User";
-
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
     const router = useRouter();
     const searchInputRef = useRef(null);
     function search(event) {
         event.preventDefault();
-        const term = searchInputRef.current;
+        const term = searchInputRef.current.value;
         if (!term.trim()) return;
-        router.push(`/search?term=${term.trim}`);
+        router.push(`/search?term=${term.trim()}&searchType=`);
     }
     return (
         <header className="sticky top-0 bg-white">
@@ -23,20 +23,26 @@ export default function SearchHeader() {
                     objectFit="contain"
                     height="40"
                     className="cursor-pointer"
-                    src="https://pbs.twimg.com/media/FmQO8zDaEAA8vdm?format=png&name=900x900" alt="" />
+                    src="https://pbs.twimg.com/media/FmQO8zDaEAA8vdm?format=png&name=900x900"
+                />
                 <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
-                    <input type="text" defaultValue={router.query.term} ref={searchInputRef}
+                    <input
+                        type="text"
+                        defaultValue={router.query.term}
+                        ref={searchInputRef}
                         className="w-full focus:outline-none"
                     />
-
-                    <XIcon onClick={() => (searchInputRef.current.value = "")} className="h-7 text-gray-500 cursor-pointer sm:mr-3" />
+                    <XIcon
+                        onClick={() => (searchInputRef.current.value = "")}
+                        className="h-7 text-gray-500 cursor-pointer sm:mr-3"
+                    />
                     <MicrophoneIcon className="h-6 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
                     <SearchIcon className="h-6 hidden sm:inline-flex text-blue-500" />
-                    <button title="." onClick={search} type="submit" hidden></button>
+                    <button onClick={search} type="submit" hidden></button>
                 </form>
                 <User className="ml-auto whitespace-nowrap" />
             </div>
-
+            <SearchHeaderOptions />
         </header>
-    )
+    );
 }
