9536c25bcd26a998a10510ff0a1fdb6f0adef322
karthik242002 <karthiksiva153@gmail.com>
2023-01-18 16:51:12 +0530

    create pagination component

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Parser from "html-react-parser"
+import PaginationButtons from './PaginationButtons'
 export default function SearchResults({ results }) {
     return (
         <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -15,6 +16,7 @@ export default function SearchResults({ results }) {
                     <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
                 </div>
             ))}
+            <PaginationButtons />
         </div>
     )
 }
