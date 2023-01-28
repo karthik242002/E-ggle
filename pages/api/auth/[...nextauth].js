384d39cef15de1142fc5d97617333fcd5317fd90
karthik242002 <karthiksiva153@gmail.com>
2023-01-28 21:30:24 +0530

    updated the button styles

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,18 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/Google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
+    // ...add more providers here
   ],
   secret: process.env.SECRET,
-  // ...add more providers here
   pages: {
-    signIn: "/auth/signin"
-  }
-}
-
-export default NextAuth(authOptions)
+    signIn: "/auth/signin",
+  },
+});
