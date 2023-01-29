3e232280b51b44683c52dca8f81d0736f5304660
karthik242002 <karthiksiva153@gmail.com>
2023-01-29 14:55:24 +0530

    updated the footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
-        <p>Copyright &copy; {new Date().getFullYear()} Karthik</p>
+    <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-0 text-sm text-gray-600">
+      <p>Copyright &copy; {new Date().getFullYear()} Karthik</p>
     </footer>
   )
 }
